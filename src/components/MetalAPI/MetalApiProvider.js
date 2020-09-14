3ee450c30036d93c473969f8bf9d57585323bdee
Michael Baumer <meeesterbaumer@gmail.com>
2020-09-14 13:58:13 -0500

    WIP

--- src/components/MetalAPI/MetalApiProvider.js
+++ src/components/MetalAPI/MetalApiProvider.js
@@ -1,2 +1,21 @@
-import key from "../../Settings.js"
+import React, { useState, useEffect } from "react"
+import apiKey from "../../Settings"
 
+export const MetalApiContext = React.createContext()
+
+export const MetalApiProvider = (props) => {
+  const [metalValue, setValue] = useState([])
+
+  const getValue = () => {
+    return fetch(`https://metals-api.com/api/latest/?access_key=${apiKey.metalKey}&base=USD&symbols=XAU,XAG,XPD,XPT,XRH`)
+    .then(r => r.json())
+    .then(setValue)
+  }
+  return (
+    <MetalApiContext.Provider value={{
+      metalValue, getValue
+    }}>
+      {props.children}
+    </MetalApiContext.Provider>
+  )
+}
