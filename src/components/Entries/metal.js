cb71bc4d4bca4d4db74597f19d9927a713543b85
Michael Baumer <meeesterbaumer@gmail.com>
2020-09-14 11:27:05 -0500

    got settings ignored and api key entered

--- src/components/Entries/metal.js
+++ src/components/Entries/metal.js
@@ -1,8 +1,12 @@
+// this component allows you to edit the individual metal entry that displays to the DOM
+
 import React from "react"
 import "./Metal.css"
 
 export const Metal = ({ metal }) => (
   <div className="metal">
     <div className="metal__name">{metal.name}</div>
+    <div className="metal__weight">{metal.weight}</div>
+<div className="metal__currentValue">Current Value goes here</div>
   </div>
 )
---
+++ src/components/MetalAPI/MetalApiProvider.js
@@ -0,0 +1,2 @@
+import key from "../../Settings.js"
+
