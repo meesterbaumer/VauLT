bc1ec1064c5a50e9ab5d109bfead4d83e3b69f0f
Michael Baumer <meeesterbaumer@gmail.com>
2020-09-15 12:01:55 -0500

    WIP

--- src/components/Entries/metalList.js
+++ src/components/Entries/metalList.js
@@ -9,16 +9,31 @@ export const MetalList = () => {
   const { metalTestValue, getTestValue } = useContext(MetalApiTestContext);
 
   useEffect(() => {
-    getMetals();
-    getTestValue();
+    getTestValue().then(getMetals);
   }, []);
   console.log(metals);
   console.log(metalTestValue);
+
+  const CollectionWeight = metals.map(metal => {
+    return metal.weight
+  })
+  console.log(CollectionWeight)
+  const collectionWeightTotal = 0
+  
+
+
   return (
-    <div className="metals">
-      {metals.map((m) => {
-        return <Metal key={m.id} metal={m} metalValue={metalTestValue} />;
-      })}
-    </div>
+    <>
+      <div className="ValueContainer">
+        <div className="collectionHeader">Collection</div>
+        <div className="collectionValue">Collection Value goes here</div>
+        <div className="collectionUpdate">timestamp goes here</div>
+      </div>
+      <div className="metals">
+        {metals.map((m) => {
+          return <Metal key={m.id} metal={m} metalValue={metalTestValue} />;
+        })}
+      </div>
+    </>
   );
 };
--- src/components/Vault.js
+++ src/components/Vault.js
@@ -13,6 +13,7 @@ export const Vault = () => (
       />
     </div>
     <NavBar />
+    
     <ApplicationViews />
   </>
 );
