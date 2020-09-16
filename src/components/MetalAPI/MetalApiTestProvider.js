e3a9f8c3f97bf119696b62408750ac4d60d658a3
Michael Baumer <meeesterbaumer@gmail.com>
2020-09-16 16:50:59 -0500

    Stopping for the night

--- src/components/MetalAPI/MetalApiTestProvider.js
+++ src/components/MetalAPI/MetalApiTestProvider.js
@@ -3,24 +3,24 @@ import React, { useState, useEffect } from "react";
 export const MetalApiTestContext = React.createContext();
 
 export const MetalApiTestProvider = (props) => {
-  const [metalTestValue, setTestValue] = useState([{"rates": {}}]);
+  const [metalTestValue, setTestValue] = useState({"rates": {}});
 
   const getTestValue = () => {
-    return fetch("http://localhost:8089/metalApiData")
-      .then((r) => r.json())
-      .then(setTestValue);
+    return fetch("https://cors-anywhere.herokuapp.com/https://metals-api.com/api/latest/?access_key=7zpizjmz4m4d0pa0qly7h1q5qmhiiv2339zicxd8p9vijcbppahrobyz37dd&base=USD&symbols=XAU%2CXAG%2CXPD%2CXPT%2CXRH")
+    .then((r) => r.json())
+    .then(setTestValue);
   };
   useEffect(() => {
     getTestValue()
   }, [])
   console.log(metalTestValue)
-
+  
   return (
     <MetalApiTestContext.Provider
-      value={{
-        metalTestValue,
-        getTestValue,
-      }}
+    value={{
+      metalTestValue,
+      getTestValue,
+    }}
     >
       {props.children}
     </MetalApiTestContext.Provider>
@@ -28,3 +28,4 @@ export const MetalApiTestProvider = (props) => {
 
 
 };
+// http://localhost:8089/metalApiData
