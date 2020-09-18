c81c9bd648b536917fb13739fc27fcfd019f4bd1
Michael Baumer <meeesterbaumer@gmail.com>
2020-09-18 15:25:42 -0500

    delete is now working

--- src/components/Entries/Metal.js
+++ src/components/Entries/Metal.js
@@ -1,17 +1,36 @@
 // this component allows you to edit the individual metal entry that displays to the DOM
 
-import React from "react";
+import React, { useContext, useRef } from "react";
+import { MetalContext } from "./MetalProvider";
 import "./Metal.css";
 
 export const Metal = ({ metal, metalValue }) => {
+  const { deleteMetals } = useContext(MetalContext);
+  const { deleteConfirmDialog } = useRef();
+
   console.log(metalValue);
-  const rates = metalValue.rates
-  const correctRate = 1/rates.XAG
+  const rates = metalValue.rates;
+  const correctRate = 1 / rates.XAG;
+
   return (
-    <div className="singleMetal">
-      <div className="metal__name">{metal.name}</div>
-  <div className="metal__weight">Weight: {metal.weight} {metal.unit.name}</div>
-      <div className="metal__currentValue">Current Value: $ {parseFloat(correctRate * metal.weight).toFixed(2)}</div>
-    </div>
+    <>
+      <div className="singleMetal">
+        <div className="metal__name">{metal.name}</div>
+        <div className="metal__weight">
+          Weight: {metal.weight} {metal.unit.name}
+        </div>
+        <div className="metal__currentValue">
+          Current Value: $ {parseFloat(correctRate * metal.weight).toFixed(2)}
+        </div>
+        <button
+          onClick={() => {
+            deleteMetals(metal.id)
+          }}
+          className="metal--delete"
+        >
+          Delete
+        </button>
+      </div>
+    </>
   );
 };
