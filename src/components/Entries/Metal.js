497b6e479e02e49f02fb8484b264c87c2393fc9b
Michael Baumer <meeesterbaumer@gmail.com>
2020-09-25 14:32:38 -0500

    got flipping coing looking 90%

--- src/components/Entries/Metal.js
+++ src/components/Entries/Metal.js
@@ -5,8 +5,6 @@ import { MetalContext } from "./MetalProvider";
 import { MetalList, editPieceButtonClicked } from "./MetalList";
 import "./Metal.css";
 
-
-
 export const Metal = ({ metal, metalValue, props }) => {
   const { deleteMetals } = useContext(MetalContext);
   // const { deleteConfirmDialog } = useRef();
@@ -18,35 +16,47 @@ export const Metal = ({ metal, metalValue, props }) => {
     <>
       <div className="singleMetal">
         <div className="metal__name">{metal.name}</div>
-        <div>
-        <img className="metal__imageFront" src={metal.imageFront} style={{width: `100px`}} alt=""></img>
-        </div>
-        <div>
-        <img className="metal__imageBack" src={metal.imageBack} style={{width: `100px`}} alt=""></img>
+        <div id="f1_container">
+          <div id="f1_card" className="shadow">
+            <div className="front face">
+              <img
+                className="metal__imageFront"
+                src={metal.imageFront}
+                style={{ height: `100px` }}
+                alt=""
+              ></img>
+            </div>
+            <div className="back face center">
+              <img
+                className="metal__imageBack"
+                src={metal.imageBack}
+                style={{ height: `100px` }}
+                alt=""
+              ></img>
+            </div>
+          </div>
         </div>
         <div className="metal__weight">
           Weight: {metal.weight} {metal.unit.name}
         </div>
-        <div className="metal__qty">
-          qty: {metal.qty}
-        </div>
+        <div className="metal__qty">qty: {metal.qty}</div>
         <div className="editDeleteButtonContainer">
-        <button
-          className="metal--edit"
-          onClick={() => {
-            props.push(`/collection/edit/${metal.id}`)
-          }}
-        >
-          Edit
-        </button>
-        <button
-          onClick={() => {
-            deleteMetals(metal.id);
-          }}
-          className="metal--delete"
-        >
-          Delete
-        </button>
+          <button
+            className="metal--edit"
+            onClick={() => {
+              props.push(`/collection/edit/${metal.id}`);
+            }}
+          >
+            Edit
+          </button>
+          <button
+            onClick={() => {
+              deleteMetals(metal.id);
+            }}
+            className="metal--delete"
+          >
+            Delete
+          </button>
         </div>
         <div className="metal__currentValue">
           Current Value: $ {parseFloat(correctRate * metal.weight).toFixed(2)}
